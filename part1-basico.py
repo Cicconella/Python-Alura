a102cd4fcd928fa6407faf738c6b1b4fc9cb67d5
cicconella <acmarc@gmail.com>
2021-01-12 14:48:50 -0300

    mini programinhas

---
+++ part1-basico.py
@@ -0,0 +1,26 @@
+
+# Imprimir texto
+
+print('amazing ana')
+print('amazing','ana', sep="+")
+
+# Variáveis
+
+nome = "ana"
+
+idade = 30
+
+print(nome, "tem", idade, "anos")
+
+# Tipo da variável
+
+print(type(nome))
+print(type(idade))
+
+# Outro exemplo
+
+dia = 3
+mes = 3
+ano = 1990
+
+print(dia, mes, ano, sep="/", end="\n")
