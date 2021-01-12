6e24f0a8625dae53f6a83d8f30be9f3ba89624b2
cicconella <acmarc@gmail.com>
2021-01-12 15:30:11 -0300

    mini programinhas

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,8 +7,31 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
-    print("Tentativa", rodada, "de", total_de_tentativas)
+#### Usando While
+
+# while (rodada <= total_de_tentativas):
+#     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+#     chute_str = input("Digite o seu número: ")
+#     chute = int(chute_str)
+#
+#     acertou = chute == numero_secreto
+#     maior = chute > numero_secreto
+#     menor = chute < numero_secreto
+#
+#     if (acertou):
+#         print("Você acertou!")
+#     else:
+#         if (maior):
+#             print("Você errou! O seu chute foi maior que o número secreto.")
+#         elif (menor):
+#             print("Você errou! O seu chute foi menor que o número secreto.")
+#
+#     rodada = rodada + 1
+
+#### Usando For
+
+for rodada in range(1,total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número: ")
     chute = int(chute_str)
 
@@ -18,6 +41,12 @@ while (rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!")
+        break
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
@@ -25,6 +54,7 @@ while (rodada <= total_de_tentativas):
             print("Você errou! O seu chute foi menor que o número secreto.")
 
     rodada = rodada + 1
+
 
 print("Fim do jogo")
 
